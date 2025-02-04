6de33ea5345fba4ffab44f2a74fe47610637ec07
yerarakaunia <yera.rakaunia11@smp.belajar.id>
2025-02-04 11:42:24 +0700

    first Upload

---
+++ catatan part7.py
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Setyo Portofolio</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Setyo Portfolio</h1>
+        <p>Welcome to my Portfolio. Nice to meet you. This's me, Web Developer.</p>
+        <img src="./asset/My Photo.png" alt="My Photo">
+        <button>See My Project</button>
+    </header>
+
+    <main>
+
+    </main>
+
+    <footer>
+
+    </footer>
+</body>
+</html>
